9ccea4de95f11fc037663cb79f3ec83f42783390
Sumit Kumar <sumitAtWork0@gmail.com>
2023-03-05 13:35:05 +0530

    Initial Commit

---
+++ ImageCompressor/ImageCompressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompressorApp.swift
+//  ImageCompressor
+//
+//  Created by Sumit Kumar on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageCompressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
